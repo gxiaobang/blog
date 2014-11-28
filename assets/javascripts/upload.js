af5d6048677de85ae1061c4f1d134aa47cac9d0c
bang <gxiaobang@163.com>
2014-11-28 17:26:49 +0800

    add sass

---
+++ assets/javascripts/upload.js
@@ -0,0 +1,49 @@
+/**
+ * 文件上传
+ * author: bang
+ * date: 2014-11-27
+ */
+
+;(function(global) {
+	"use strict";
+
+var 
+	// 文件上传
+	upload,
+	// 请求
+	http;
+
+http = function(url, data) {
+	// 创建
+	var xhr = new XMLHttpRequest();
+	// 监听返回
+	xhr.onreadystatechange = function() {
+		// console.log(this.readyState);
+		switch (this.readyState) {
+			// 请求未初始化
+			case 0:
+				break;
+			// 请求已经建立
+			case 1:
+				break;
+			// 请求已发送
+			case 2:
+				break;
+			// 请求处理中
+			case 3:
+				break;
+			// 响应已完成
+			case 4:
+				break;
+		}
+	};
+	// 打开
+	xhr.open('POST', url, true);
+	// 发送
+	xhr.send(data);
+};
+
+upload = function(data) {
+	data = dataFormat(data);
+};
+})(this);
